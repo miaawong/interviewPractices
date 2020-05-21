1106d42c2491d3cb8e38d16868321e99bbb3e13f
Mia Wong <miaw629@gmail.com>
2020-05-21 11:47:51 -0400

    5/21 algo cardio

--- algo/traversyCardio.js
+++ algo/traversyCardio.js
@@ -16,4 +16,66 @@ const reverseString = (str) => {
         );
 };
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+const isPalindrome = (str) => {
+    //return true if its a palindrome, false if not
+    // ex: racecar === true
+    const revStr = str.split("").reverse().join("");
+    return revStr === str ? "true" : "false";
+};
+
+// console.log(isPalindrome("hello"));
+
+const reverseInt = (int) => {
+    //return an integer in reverse
+    let revStr = int.toString().split("").reverse().join("");
+    return parseInt(revStr) * Math.sign(int);
+};
+
+// console.log(reverseInt(-678));
+
+const capitalizeLetters = (str) => {
+    //return a string with the first letter of every word capitalized
+    //split the string between the spaces
+    //get first letter of that
+    // then cap it
+    //join it
+    // mia's first way
+    return (
+        str
+            .split(" ")
+            // word[0] is a string! but I am using Property Access (es5)
+            // I also used charAt
+            .map((word) => word[0].toUpperCase() + word.substring(1))
+            .join(" ")
+    );
+};
+// console.log(capitalizeLetters("i love javascript"));
+
+const maxCharacter = (str) => {
+    //this one was challenging cause i ddidn't know how to add to an object?
+    // return a character that occurs the most time in this string
+    //ex: 'hello' == 'l'
+    let maxChar = "";
+    let arr = str.split("");
+    let charMap = {};
+    let max = 0;
+
+    arr.map((char) => {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    });
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+};
+
+console.log(maxCharacter("javascript"));
